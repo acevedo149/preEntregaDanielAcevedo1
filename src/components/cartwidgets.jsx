fbf3197245022706010944e76385148cab569d9d
daniel acevedo <acevedo149@hotmail.com>
2022-11-30 23:04:57 -0500

    first commit

---
+++ src/components/cartwidgets.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const CartWidgts = () => {
+    return(
+        <button type="button" className="btn areapublicidad position-relative">
+            <i className="bi bi-cart4"></i>
+  Inbox
+  <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+    1
+  </span>
+</button>
+
+    )
+}
+
+export default CartWidgts;
---
+++ src/components/promocion.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const promocion = () => {
+    return(
+        <div className="row">
+            <div className="col-md-6">
+                <img src="images\wrench.webp" alt="wrench" width={350} />
+            </div>
+            <div className="col-md-6">
+                <h3>gran promocion compra una y obten la otra con el 50% off</h3>
+            </div>
+        </div>
+    )
+}
+
+export default promocion;
